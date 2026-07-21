7b2df9557809dcecf77c1a96b417c03acc807ae1
Aboubacar-Coulibaly <coulibaly7988@gmail.com>
2026-07-21 18:55:05 +0000

    last commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import {Citation} from './Citation.js'
 
 const stat = document.querySelector(".stat");
-const btn =document.createElement("button");
+console.log(stat);
+
+const btn = document.createElement("button");
 btn.textContent ="Generate";
 btn.classList.add("button");
 stat.appendChild(btn);
@@ -14,9 +16,12 @@ btn.addEventListener("click",generate);
 
  function generate(){
      let i= parseInt(Math.random()*6);
-    p.textContent = Sitation[i].description;
-    h3.textContent = Sitation[i].auteur;
+    p.textContent = Citation[i].description;
+    h3.textContent = Citation[i].auteur;
+    if(stat.contains(h3) && stat.contains(p)){
+        stat.removeChild(h3);
+        stat.removeChild(p);
+    }
     stat.insertBefore(h3,btn);
     stat.insertBefore(p,btn);
-   
  }
